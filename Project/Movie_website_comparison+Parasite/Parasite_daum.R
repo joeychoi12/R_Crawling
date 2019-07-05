437e17eb016abdbf2588820748b57dd48de5958a
joeychoi12 <joeychoi12@gmail.com>
2019-07-05 17:55:00 +0900

    Crawling Project

--- Project/Movie_website_comparison+Parasite/Parasite_daum.R
+++ Project/Movie_website_comparison+Parasite/Parasite_daum.R
@@ -18,9 +18,6 @@ content_test <- html_node(html_test,".review_info")
 grade <- html_nodes(html_test,".emph_grade") %>% html_text()
 date <- html_nodes(html_test,".info_append") %>% html_text() %>% trim3() %>% trim4() %>% trim2()
 
-
-
-date <- 
 content <- html_nodes(htxt,".review_info") 
 
 daum.grade <- c()
@@ -31,10 +28,11 @@ daum.date <- c()
 for (i in 1:625) {
   if (i %% 100 == 0) print(i)
   html <- read_html(paste0(url,i))
-  daum.grade <-  c(daum.grade,html_nodes(html_test,".emph_grade") %>% html_text())
+  daum.grade <-  c(daum.grade,html_nodes(html,".emph_grade") %>% html_text())
   daum.review <- c(daum.review,html_nodes(html,".desc_review") %>% html_text() %>% trim3() %>% trim4() %>% trim2())
   daum.date <- c(daum.date, html_nodes(html,".info_append") %>% html_text() %>% trim3() %>% trim4() %>% trim2())
 }
+tail(daum.review)
 daum.grade[6250] <- NA
 daum.review[6250] <- NA
 daum.date[6250] <- NA
@@ -43,14 +41,18 @@ str(daum.review)
 str(daum.date)
 df_daum.review <-  data.frame(rating = daum.grade, user_review = daum.review, date = daum.date)
 
+write.xlsx(df_daum.review,"다음 기생충 평점.xlsx")
+mean(as.numeric(as.character(df_daum.review$rating)))
+mean(as.numeric((as.character(daum.grade))))
+
 View(df_daum.review)
 
 
 # Word cloud of review words to see what words user used to describe the movie 
 library(wordcloud2)
 library(KoNLP)
-write(daum.review, "daum_review.txt")
-data <- readLines('daum_review.txt') 
+write(daum.review, "daum_review1.txt")
+data <- readLines('daum_review1.txt') 
 data1 <- sapply(data, extractNoun, USE.NAMES = F)
 
 
@@ -65,8 +67,8 @@ for(i in 1:length(txt2)) {
   data4 <- gsub(txt2[i],"",data3)
 }
 
-write(data4,"parasite.txt")
-data5 <- read.table("parasite.txt")
+write(data4,"parasite_daum.txt")
+data5 <- read.table("parasite_daum.txt")
 nrow(data5)
 wordcount <- table(data5)
 wordcloud <-sort(wordcount,decreasing = T)
@@ -81,54 +83,76 @@ wordcloud2(wordcloud)
 # Plot of user rating and time to see how the user rating went as the movie came out
 library(ggplot2)
 theme_set(theme_classic()) # change the theme to my preferece 
+View(df_daum.review)
+df_daum.review$rating <- as.numeric(as.character(df_daum.review$rating))
+names(df_daum.review) <- c("rating"   ,   "user_review", "date_time"  )
+df_daum.review$date <- substr(df_daum.review$date_time,1,10)
+df_daum.review$time <- substr(df_daum.review$date_time,13,17)
+df_daum.review$date <-as.Date(df_daum.review$date, "%Y.%m.%d")
 time.sort <- df_daum.review %>% 
   select(rating,date) %>%
   group_by(date) %>%
   summarise(review_mean = mean(as.numeric(rating)))
-df_daum.review$rating <- as.numeric(as.character(df_daum.review$rating))
-df_daum.review$time <- substr(df_daum.review$date_time,13,18)
-df_daum.review$time
-time.sort
-class(df_daum.review$date)                  
-df_daum.review$date <-as.Date(df_daum.review$date, "%Y.%m.%d")
-df_daum.review$time <- as.Time
-
+View(time.sort)
 # line Plot from 2017 way before the movie was released 
 theme_set(theme_grey()) # change the theme to my preferece 
-ggplot(aes(x = date, y = review_mean), data = time.sort) + geom_line() + geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red")
+ggplot(aes(x = date, y = review_mean), data = time.sort) + geom_line(col="blue") + 
+  geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red") + 
+  ggtitle("[다음]기생충 2017년 부터 네티즌 평가") +
+  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 15, color = "darkblue"))
+  
+  
 ps.release.date <- as.Date("05.30.2019","%m.%d.%Y")
 
 
 # line Plot from 3 days before the movie was released
 time.sort.2019 <- time.sort %>%
-  filter(date >= "2019-05-27")
-ggplot(aes(x = date, y = review_mean), data = time.sort.2019) + 
-  geom_line(color = "blue") + 
+  filter(date >= "2019-05-26")
+daum.p1 <- ggplot(aes(x = date, y = review_mean), data = time.sort.2019) + 
+  geom_line(color = "blue") + geom_point() +
   geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red") +
-  ggtitle("[다음] 개봉 이후 네티즌 평점")
+  ggtitle("[Daum] 개봉 이후 네티즌 평점") +   theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 15, color = "darkblue"))+
+  geom_label(aes(x=ps.release.date,label="개봉일",y=9.5))
+daum.p1
+  
 ps.release.date <- as.Date("05.30.2019","%m.%d.%Y")
 
-names(df_daum.review) <- c("rating", "user_review","date_time",  "date"   )
-df_daum.review$date2 <- substr(df_daum.review$date,1,10)
 
 
-# bar graph of time and review
+### bar graph of time and review
 time.sort.release <- time.sort %>%
   filter(date >= "2019-05-30")
-ggplopt(time.sort.release,aes())
 ggplot(time.sort.release, aes(date,review_mean)) + geom_col()
 
+## Line graph of time and review
+df_daum.review$hour <- substr(df_daum.review$time,1,2)
+
+#df_daum.review$rating <- as.numeric(as.character(df_daum.review$rating))
+sort.byTime <- df_daum.review %>% 
+  select(rating,hour) %>%
+  group_by(hour)%>%
+  summarise(rating = mean(rating))
+sort.byTime$hour <- as.numeric(sort.byTime$hour)
+class(sort.byTime$hour)
+View(sort.byTime)
+
+daum.p2 <- ggplot(sort.byTime,aes(hour,rating,group=1)) + geom_point() + geom_line(color="red") + 
+  ggtitle("[Daum] 개봉 이후 시간별 네티즌 평점 변화 그래프") + 
+  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 15, color = "darkblue"))
+daum.p2
+
+
 # bar graph of day and num of people who wrote reviews
-time.sort.people <- df_daum.review %>% 
-  filter(date >= "2019-05-21") %>%
+sort.byPeople <- df_daum.review %>% 
+  filter(date >= "2019-05-30") %>%
   select(rating,date,time) %>%
   group_by(date) %>%
   summarise(Count = n(), Rating = mean(rating))
-View(time.sort.people)
-View(time.sort.people)
-ggplot(time.sort.people, aes(date,Count,fill=Rating)) + geom_col() + geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red",label = "Release Date") +
+View(sort.byPeople)
+daum.p3 <- ggplot(sort.byPeople, aes(date,Count,fill=Rating)) + geom_col() + geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red",label = "Release Date") +
   scale_fill_gradient(low="blue", high="red") + ggtitle("Number of Reviews written per day: [Daum Movie]") +
-  geom_label(aes(x=ps.release.date, label="Release Date \n 692 People", y=760), colour="black", text=element_text(size=11),fill = "white")
+  geom_label(aes(x=ps.release.date, label="Release Date \n 692 People", y=760), colour="black", size = 3,fill = "white")
+daum.p3
 # line plot of time and review
 colfunc <- colorRampPalette("blue")
 
@@ -151,3 +175,7 @@ sort.people.time <- sort.people.time[2:333,]
 ggplot(sort.people.time, aes(hour,Count)) + geom_col() + geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red",label = "Release Date") +
   scale_fill_gradient(low="blue", high="red") + ggtitle("Number of Reviews written per time") +
   geom_text(aes(x=ps.release.date, label="Release Date \n 692 People", y=730), colour="black", text=element_text(size=11))
+
+daum.p1
+daum.p2
+daum.p3
--- Project/Movie_website_comparison+Parasite/Parasite_naver.R
+++ Project/Movie_website_comparison+Parasite/Parasite_naver.R
@@ -5,14 +5,20 @@ library(rvest)
 library(stringr)
 library(XML)
 library(dplyr)
+library(extrafont)
+windowsFonts()
+windowsFonts(malgun = "맑은 고딕")
+theme_update(text = element_text(family = "malgun"))
+
+
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 trim2 <- function(x) gsub("\\n","",x)
 trim3 <- function(x) gsub("\\t","",x)
 trim4 <- function(x) gsub("\\r","",x)
 
 url <- "https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?code=161967&type=after&isActualPointWriteExecute=false&isMileageSubscriptionAlready=false&isMileageSubscriptionReject=false&page="
-url
-html_test <- read_html(paste0(url,ifr,page,1))
+#url
+#html_test <- read_html(paste0(url,ifr,page,1))
 
 rating <- c()
 review <- c()
@@ -48,16 +54,15 @@ for (i in 1:2929) {
 df.review <- data.frame(rating = rating, user_review = review, date = date)
 df_naver.review <- df.review
 df_naver.review$user_review
-names()
+head(df_naver.review)
 View(df_naver.review)
 
 View(review)
 
-
 # Word cloud of review words to see what words user used to describe the movie 
 library(wordcloud2)
 library(KoNLP)
-write(review, "naver_review.txt")
+write(review, "naver_review1.txt")
 data <- readLines('naver_review.txt')
 data1 <- sapply(data, extractNoun, USE.NAMES = F)
 
@@ -69,74 +74,86 @@ data4 <- str_replace_all(data3, "[^[:alpha:]]","") #한글, 영어이외는 삭�
 
 txt2 <- readLines("네이버영화gsub.txt")
 for(i in 1:length(txt2)) {
-  data4 <- gsub(txt2[i],"",data3)
+  data5 <- gsub(txt2[i],"",data4)
 }
-length(txt2)
+txt2[1]
+data5 <- gsub("영화","",data5)
+data5 <- gsub("관람객","",data5)
 
-
-write(data4,"parasite_Naver.txt")
-data5 <- read.table("parasite_Naver.txt")
-nrow(data5)
-wordcount <- table(data5)
+write(data5,"parasite_Naver2.txt")
+data6 <- read.table("parasite_Naver2.txt")
+nrow(data6)
+wordcount <- table(data6)
 wordcloud <-sort(wordcount,decreasing = T)
 head(wordcloud)
 
 wordcloud2(wordcloud)
 
-
-
-
-
 # Plot of user rating and time to see how the user rating went as the movie came out
 library(ggplot2)
-theme_set(theme_classic()) # change the theme to my preferece 
-time.sort <- df_daum.review %>% 
+#theme_set(theme_classic()) # change the theme to my preferece 
+
+View(df_naver.review)
+df_naver.review$rating <- as.numeric(as.character(df_naver.review$rating))
+names(df_naver.review) <- c("rating"   ,   "user_review", "date_time"    )
+df_naver.review$date <- substr(df_naver.review$date,1,10)
+df_naver.review$time <- substr(df_naver.review$date_time,12,16)
+df_naver.review$hour <- substr(df_naver.review$date_time,12,13)
+
+View(df_naver.review)
+time.sort <- df_naver.review %>% 
   select(rating,date) %>%
   group_by(date) %>%
   summarise(review_mean = mean(as.numeric(rating)))
-df_daum.review$rating <- as.numeric(as.character(df_daum.review$rating))
-df_daum.review$time <- substr(df_daum.review$date_time,13,18)
-df_daum.review$time
-time.sort
-class(df_daum.review$date)                  
-df_daum.review$date <-as.Date(df_daum.review$date, "%Y.%m.%d")
-df_daum.review$time <- as.Time
+View(time.sort)
+df_naver.review$date <-as.Date(df_naver.review$date, "%Y.%m.%d")
 
-# line Plot from 2017 way before the movie was released 
-theme_set(theme_grey()) # change the theme to my preferece 
-ggplot(aes(x = date, y = review_mean), data = time.sort) + geom_line() + geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red")
-ps.release.date <- as.Date("05.30.2019","%m.%d.%Y")
-
-
-# line Plot from 3 days before the movie was released
-time.sort.2019 <- time.sort %>%
-  filter(date >= "2019-05-27")
-ggplot(aes(x = date, y = review_mean), data = time.sort.2019) + 
+# line Plot of rating per day from release date
+naver.p1 <- ggplot(time.sort,aes(x = date, y = review_mean,group = 1)) + 
+  geom_point() +
   geom_line(color = "blue") + 
-  geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red") +
-  ggtitle("[다음] 개봉 이후 네티즌 평점")
-ps.release.date <- as.Date("05.30.2019","%m.%d.%Y")
+  theme(axis.text.x = element_text(size = 8, angle = 90)) +
+  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 15, color = "darkblue")) + 
+  ylab("평점") + xlab("")
 
-names(df_daum.review) <- c("rating", "user_review","date_time",  "date"   )
-df_daum.review$date2 <- substr(df_daum.review$date,1,10)
+View(df_naver.review)
+# Line graph of time and review
+df_naver.review$hour <- substr(df_naver.review$time,1,2)
+df_naver.review$time
+sort.byTime <- df_naver.review %>% 
+  select(rating,time,hour) %>%
+  group_by(hour)%>%
+  summarise(rating = mean(rating))
+class(sort.byTime$hour)
+sort.byTime$hour <- as.numeric(sort.byTime$hour)
+View(sort.byTime)
 
+naver.p2 <- ggplot(sort.byTime,aes(hour,rating,group=1)) + geom_point() + geom_line(color="red") + 
+  ggtitle("[네이버] 개봉 이후 시간별 네티즌 평점 변화 그래프") + 
+  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 15, color = "darkblue"))
 
-# bar graph of time and review
-time.sort.release <- time.sort %>%
-  filter(date >= "2019-05-30")
-ggplopt(time.sort.release,aes())
 ggplot(time.sort.release, aes(date,review_mean)) + geom_col()
 
 # bar graph of day and num of people who wrote reviews
-time.sort.people <- df_daum.review %>% 
-  filter(date >= "2019-05-21") %>%
+sort.byPeople <- df_naver.review %>% 
   select(rating,date,time) %>%
   group_by(date) %>%
   summarise(Count = n(), Rating = mean(rating))
-View(time.sort.people)
-ggplot(time.sort.people, aes(date,Count,fill=Rating)) + geom_col() + geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red",label = "Release Date") +
-  scale_fill_gradient(low="blue", high="red") + ggtitle("Number of Reviews written per day: [Daum Movie]") +
-  geom_label(aes(x=ps.release.date, label="Release Date \n 692 People", y=760), colour="black", text=element_text(size=11),fill = "white")
-# line plot of time and review
-colfunc <- colorRampPalette("blue")
+View(sort.byPeople)
+firstday <- sort.byPeople$Count[1]
+firstday
+ps.release.date <- as.Date("2019-05-30","%Y-%m-%d")
+naver.p3 <- ggplot(sort.byPeople, aes(date,Count,fill=Rating,)) + geom_col() + 
+  scale_fill_gradient(low="blue", high="red") + ggtitle("[네이버] 기생충 날짜별 네티즌 리뷰 건수") + 
+  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 15, color = "darkblue")) +
+  geom_vline(xintercept = ps.release.date, linetype="dashed", color = "red",label = "Release Date") +
+  geom_label(aes(x=sort.byPeople$date[1], label="개봉일 \n 3340 명", y=3800), colour="black", size = 2.8,fill = "white")
+
+install.packages("xlsx")
+library(xlsx)
+
+
+write.csv(df_naver.review,"네이버평점_df")
+write.xlsx(df_naver.review,file = "naver_Parasite.xlsx")
+
 
